248f6b495f71dc21e4c3123ab1a5626b50362375
Daniel Gibim <DanielGibimdev@gmail.com>
2025-06-10 15:41:16 -0300

    feat: adiciona construtores com WebDriver e WebDriverWait em CadastroPage e EditarDeletarPage

--- src/main/java/pages/CadastroPage.java
+++ src/main/java/pages/CadastroPage.java
@@ -1,4 +1,17 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class CadastroPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public CadastroPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+
 }
--- src/main/java/pages/EditarDeletarPage.java
+++ src/main/java/pages/EditarDeletarPage.java
@@ -1,4 +1,17 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class EditarDeletarPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public EditarDeletarPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+
 }
